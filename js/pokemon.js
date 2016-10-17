31b59506c561394c54655a9a6693dda1968095f4
Christopher Collins <chris@spring215.com>
2016-10-17 18:28:06 -0400

    init push

---
+++ js/pokemon.js
@@ -0,0 +1,50 @@
+
+
+var bulbasaur = {
+	number: "001",
+	  name: "Bulbasaur",
+	 type1: "grass",
+	 type2: "poison",
+	 weak1: "fire",
+	 weak2: "flying",
+	 weak3: "ice",
+	 weak4: "psychic"
+}
+
+var ivysaur = {
+	number: "002",
+	  name: "Ivysaur",
+	 type1: "grass",
+	 type2: "poison",
+	 weak1: "fire",
+	 weak2: "flying",
+	 weak3: "ice",
+	 weak4: "psychic"
+}
+
+var venusaur = {
+	number: "003",
+	  name: "Venusaur",
+	 type1: "grass",
+	 type2: "poison",
+	 weak1: "fire",
+	 weak2: "flying",
+	 weak3: "ice",
+	 weak4: "psychic"
+}
+
+var charmander = {
+	number: "004",
+	  name: "Charmander",
+	 type1: "fire",
+	 type2: "fire",
+	 weak1: "rock",
+	 weak2: "ground",
+	 weak3: "water",
+	 weak4: "psychic"
+}
+
+function init() {
+	alert(charmander.name)
+}
+
